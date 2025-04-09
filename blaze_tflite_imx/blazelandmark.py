a9b8548a4933a917d4ab7ca5e5fb26afb83e695e
zebular13 <monica.s.houston@gmail.com>
2025-04-09 14:26:46 -0500

    Added blaze_tflite_imx with support for i.mx93 processor

---
+++ blaze_tflite_imx/blazelandmark.py
@@ -0,0 +1,158 @@
+import numpy as np
+
+from blazebase import BlazeLandmarkBase
+
+from timeit import default_timer as timer
+
+class BlazeLandmark(BlazeLandmarkBase):
+    def __init__(self,blaze_app="blazeposelandmark", delegate_path=None, quantized = False):
+        super(BlazeLandmark, self).__init__()
+
+        self.blaze_app = blaze_app
+        self.delegate_path = delegate_path
+        self.quantized = quantized
+
+    def load_model(self, model_path):
+
+        import tflite_runtime.interpreter as tflite
+           
+        if(self.delegate_path):
+            ext_delegate = [tflite.load_delegate(self.delegate_path)]
+            self.interp_landmark = tflite.Interpreter(model_path=model_path, experimental_delegates=ext_delegate)
+        else:
+            self.interp_landmark = tflite.Interpreter(model_path=model_path)
+        self.interp_landmark.allocate_tensors()
+
+        # reading tflite model paramteres
+        self.input_details = self.interp_landmark.get_input_details()
+        self.output_details = self.interp_landmark.get_output_details()
+        self.num_inputs = len(self.input_details)
+        self.num_outputs = len(self.output_details)      
+        if self.DEBUG:
+           print("[BlazeLandmark.load_model] Number of Inputs : ",self.num_inputs)
+           for i in range(self.num_inputs):
+               #print("[BlazeLandmark.load_model] Input[",i,"] Details : ",self.input_details[i])
+               print("[BlazeLandmark.load_model] Input[",i,"] Shape : ",self.input_details[i]['shape']," (",self.input_details[i]['name'],") Quantization : ",self.input_details[i]['quantization'])          
+           print("[BlazeLandmark.load_model] Number of Outputs : ",self.num_outputs)
+           for i in range(self.num_outputs):
+               #print("[BlazeLandmark.load_model] Output[",i,"] Details : ",self.output_details[i])
+               print("[BlazeLandmark.load_model] Output[",i,"] Shape : ",self.output_details[i]['shape']," (",self.output_details[i]['name'],") Quantization : ",self.output_details[i]['quantization'])          
+                
+         ## use these for 
+         ## Pose: pose_detection.tflite + pose_landmark_full.tflite/pose_landmark_lite.tflite, pose_detection_quant_floatinputs_sramonly_vela.tflite, pose_landmark_v0_07_upper_body.tflite
+         ## Face: face_landmark_pixabay_unsignedquant
+        if self.quantized==False or self.blaze_app == "blazefacelandmark":
+            self.in_idx = self.input_details[0]['index']
+            self.out_landmark_idx = self.output_details[0]['index']
+            self.out_flag_idx = self.output_details[1]['index']
+
+            self.in_shape = self.input_details[0]['shape']
+            self.out_landmark_shape = self.output_details[0]['shape']
+            self.out_flag_shape = self.output_details[1]['shape']
+
+        ## use these for []quant_floatinputs.tfllite models
+        else:
+            self.in_idx = self.input_details[0]['index']
+            self.out_landmark_idx = self.output_details[3]['index']
+            self.out_flag_idx = self.output_details[1]['index']
+
+            self.in_shape = self.input_details[0]['shape']
+            self.out_landmark_shape = self.output_details[3]['shape']
+            self.out_flag_shape = self.output_details[1]['shape']
+
+        ## use these for pose_detection.tflite + pose_landmark_full.tflite, []quant_floatinputs_vela.tflite models, 
+        ## facae detectio kaggle_quant
+        # self.in_idx = self.input_details[0]['index']
+        # self.out_landmark_idx = self.output_details[1]['index']
+        # self.out_flag_idx = self.output_details[2]['index']   
+        
+        # self.in_shape = self.input_details[0]['shape']
+        # self.out_landmark_shape = self.output_details[1]['shape']
+        # self.out_flag_shape = self.output_details[2]['shape']   
+
+
+        if self.DEBUG:
+          print("[BlazeLandmark.load_model] Input Shape : ",self.in_shape)
+          print("[BlazeLandmark.load_model] Output1 Shape : ",self.out_landmark_shape)
+          print("[BlazeLandmark.load_model] Output2 Shape : ",self.out_flag_shape)
+
+        self.resolution = self.in_shape[1]
+
+    def preprocess(self, x):
+        # image was already pre-processed by extract_roi in blaze_common/blazebase.py
+        # format = RGB
+        # dtype = float32
+        # range = 0.0 - 1.0
+        return x
+
+    def predict(self, x):
+
+        self.profile_pre = 0.0
+        self.profile_model = 0.0
+        self.profile_post = 0.0
+
+        out1_list = []
+        out2_list = []
+        #out3_list = []
+
+        #print("[BlazeLandmark] x ",x.shape,x.dtype)
+        start = timer()        
+        x = self.preprocess(x)
+        self.profile_pre += timer()-start
+                
+        nb_images = x.shape[0]
+        for i in range(nb_images):
+
+            start = timer()
+            xi = np.expand_dims(x[i,:,:,:], axis=0)
+            #print("[BlazeLandmark] xi ",xi.shape,xi.dtype)
+
+            # 1. Preprocess the images into tensors:
+            self.interp_landmark.set_tensor(self.in_idx, xi)
+            self.profile_pre += timer()-start
+                               
+            # 2. Run the neural network:
+            start = timer()  
+            self.interp_landmark.invoke()
+            self.profile_model += timer()-start
+
+            start = timer()  
+
+            if self.blaze_app == "blazehandlandmark":
+                out1 = np.asarray(self.interp_landmark.get_tensor(self.out_flag_idx))
+                out2 = np.asarray(self.interp_landmark.get_tensor(self.out_landmark_idx))
+                out2 = out2.reshape(1,21,-1) # 42 => [1,21,2] / 63 => [1,21,3]
+                out2 = out2/self.resolution
+                #out3 = np.zeros(out1.shape,out1.dtype) # tflite model not returning handedness
+            elif self.blaze_app == "blazefacelandmark":
+                out1 = np.asarray(self.interp_landmark.get_tensor(self.out_flag_idx))
+                out1 = out1.reshape(1,1)
+                out2 = np.asarray(self.interp_landmark.get_tensor(self.out_landmark_idx))
+                out2 = out2.reshape(1,-1,3) # 1404 => [1,356,2]
+                out2 = out2/self.resolution            
+            elif self.blaze_app == "blazeposelandmark":
+                out1 = np.asarray(self.interp_landmark.get_tensor(self.out_flag_idx))
+                out2 = np.asarray(self.interp_landmark.get_tensor(self.out_landmark_idx))
+                if out2.shape[1] == 124:
+                    out2 = out2.reshape(1,-1,4) # v0.07 upper : 124 => [1,31,4]
+                else:
+                    out2 = out2.reshape(1,-1,5) # v0.10 full  : 195 => [1,39,5]
+                out2 = out2/self.resolution
+                #out3 = np.asarray(self.interp_poselandmark.get_tensor(self.out_seg_idx))
+
+
+            out1_list.append(out1.squeeze(0))
+            out2_list.append(out2.squeeze(0))
+            #out3_list.append(out3.squeeze(0))
+            self.profile_post += timer()-start
+
+
+        flag = np.asarray(out1_list)
+        landmarks = np.asarray(out2_list)        
+
+        if self.DEBUG:
+            print("[BlazeLandmark] flag ",flag.shape,flag.dtype)
+            print("[BlazeLandmark] flag Min/Max: ",np.amin(flag),np.amax(flag))
+            print("[BlazeLandmark] landmarks ",landmarks.shape,landmarks.dtype)
+            print("[BlazeLandmark] landmarks Min/Max: ",np.amin(landmarks),np.amax(landmarks))
+        return flag,landmarks
